1b57f3d664f5c72c82535f083424ba73129f886d
Kavrenoo <lvanderroest1@gmail.com>
2026-08-15 23:43:13 +0000

    Add files via upload

--- public/Prism Main.lua
+++ public/Prism Main.lua
@@ -2760,14 +2760,24 @@ PM.createMainGUI = function()
                             
                             -- Fast flicker burst
                             local fastFlickerCount = math.random(2, 4)
-                            for _ = 1, fastFlickerCount do
-                                wait(math.random(0.05, 0.1))
-                                tagLbl.TextTransparency = 1
-                                wait(math.random(0.02, 0.05))
-                                tagLbl.TextTransparency = 0
+                            for i = 1, fastFlickerCount do
+                                for _ = 1, fastFlickerCount do
+                                    wait(math.random(0.05, 0.1))
+                                    tagLbl.TextTransparency = 1
+                                    wait(math.random(0.02, 0.05))
+                                    tagLbl.TextTransparency = 0
+                                end
+                                
+                                -- Single slow flicker between fast bursts
+                                if i < fastFlickerCount then
+                                    wait(math.random(0.2, 0.4))
+                                    tagLbl.TextTransparency = 1
+                                    wait(math.random(0.1, 0.2))
+                                    tagLbl.TextTransparency = 0
+                                end
                             end
                             
-                            -- Slow flicker phase between bursts
+                            -- Slow flicker phase after all bursts
                             local slowFlickerCount = math.random(3, 6)
                             for _ = 1, slowFlickerCount do
                                 wait(math.random(0.3, 0.6))
