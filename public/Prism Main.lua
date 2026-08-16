677933db837b3e4beed122fdf4d462675a6c33b6
Kavrenoo <lvanderroest1@gmail.com>
2026-08-16 01:23:43 +0000

    Add files via upload

--- public/Prism Main.lua
+++ public/Prism Main.lua
@@ -2439,10 +2439,10 @@ PM.createMainGUI = function()
         
         -- Custom Tags Configuration
         PM.CustomTags = {
-            -- Add custom tags here: [UserId] = {tagText = "Custom Name", effect = "typing" | "glitch" | "flicker"}
-            [5712636024] = {tagText = "Prism Owner", effect = "glitch"},
-            [11087809132] = {tagText = "Prism Owner", effect = "glitch"},
-            [2326644104] = {tagText = "Prism Co-Owner", effect = "flicker"},
+            -- Add custom tags here: [UserId] = {tagText = "Custom Name", effect = "typing" | "glitch" | "flicker", customPfp = "URL"}
+            [5712636024] = {tagText = "Prism Owner", effect = "glitch", customPfp = "https://raw.githubusercontent.com/Kavrenoo/Prism/main/nametag%20profile%20pictures/8241-black-and-white-cat-828907098.jpg"},
+            [11087809132] = {tagText = "Prism Owner", effect = "glitch", customPfp = "https://raw.githubusercontent.com/Kavrenoo/Prism/main/nametag%20profile%20pictures/8241-black-and-white-cat-828907098.jpg"},
+            [2326644104] = {tagText = "Prism Co-Owner", effect = "flicker", customPfp = "https://raw.githubusercontent.com/Kavrenoo/Prism/main/nametag%20profile%20pictures/8241-black-and-white-cat-828907098.jpg"},
         }
         
         PM.nametagsEnabled = true
@@ -2624,7 +2624,11 @@ PM.createMainGUI = function()
             
             spawn(function()
                 local ok, img = pcall(function()
-                    return Players:GetUserThumbnailAsync(plr.UserId, Enum.ThumbnailType.AvatarBust, Enum.ThumbnailSize.Size100x100)
+                    if customConfig and customConfig.customPfp then
+                        return customConfig.customPfp
+                    else
+                        return Players:GetUserThumbnailAsync(plr.UserId, Enum.ThumbnailType.AvatarBust, Enum.ThumbnailSize.Size100x100)
+                    end
                 end)
                 if ok and img and pfp.Parent then pfp.Image = img end
             end)
